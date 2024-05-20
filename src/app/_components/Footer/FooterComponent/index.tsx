aabfc89299425677001c621317c60427fdbc0afb
Kunal <rajpal.kunal1@gmail.com>
2024-05-19 23:49:38 -0400

    Initial footer commit

---
+++ src/app/_components/Footer/FooterComponent/index.tsx
@@ -0,0 +1,10 @@
+import React from 'react'
+
+import classes from './index.module.scss'
+
+// const FooterComponent = () => {
+//   return (<footer}></footer>);
+// }
+
+//
+export default FooterComponent
--- src/app/constants/index.ts
+++ src/app/constants/index.ts
@@ -2,22 +2,22 @@ export const inclusions = [
   {
     title: 'Free Shipping',
     description: 'Free shipping for order above $150',
-    icon: '/assets/icons/shipping.svg',
+    //icon: '/assets/icons/shipping.svg',
   },
   {
     title: 'Money Guarantee',
     description: 'Within 30 days for an exchange',
-    icon: '/assets/icons/dollar.svg',
+    //icon: '/assets/icons/dollar.svg',
   },
   {
     title: 'Online Support',
     description: '24 hours a day, 7 days a week',
-    icon: '/assets/icons/support.svg',
+    //icon: '/assets/icons/support.svg',
   },
   {
     title: 'Flexible Payment',
     description: 'Pay with multiple credit cards',
-    icon: '/assets/icons/payment.svg',
+    //icon: '/assets/icons/payment.svg',
   },
 ]
 
@@ -25,22 +25,22 @@ export const profileNavItems = [
   {
     title: 'Personal Information',
     url: '/account',
-    icon: '/assets/icons/user.svg',
+    //icon: '/assets/icons/user.svg',
   },
   {
     title: 'My Purchases',
     url: '/account/purchases',
-    icon: '/assets/icons/purchases.svg',
+    //icon: '/assets/icons/purchases.svg',
   },
   {
     title: 'My Orders',
     url: '/account/orders',
-    icon: '/assets/icons/orders.svg',
+    //icon: '/assets/icons/orders.svg',
   },
   {
     title: 'Logout',
     url: '/logout',
-    icon: '/assets/icons/logout.svg',
+    //icon: '/assets/icons/logout.svg',
   },
 ]
 
